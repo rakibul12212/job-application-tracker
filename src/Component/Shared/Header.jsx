b52321d1cfc3a86ac58b10d53f52325481d31156
rakib12212 <rakibulislamr83@gmail.com>
2025-05-05 12:02:35 +0600

    initial setup project

---
+++ src/Component/Shared/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="sticky top-0 z-1">
+      <p className="text-2xl font-semibold text-center py-5 bg-black text-white">Job Application Tracker</p>
+    </div>
+  );
+};
+
+export default Header;
